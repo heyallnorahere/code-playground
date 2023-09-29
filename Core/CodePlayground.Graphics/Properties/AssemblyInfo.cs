133362f19909ea255f6a73ea8bf165348823fd9f
yodasoda1219 <nora.github@bedafamily.com>
2023-09-29 16:33:36 -0700

    what? no

--- Core/CodePlayground.Graphics/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-#if IOS
-using ObjCRuntime;
-#endif
-
-#if IOS
-[assembly: LinkWith("libSDL2.a", LinkTarget.x86_64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
-[assembly: LinkWith("libSDL2.a", LinkTarget.x86_64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
-#endif
