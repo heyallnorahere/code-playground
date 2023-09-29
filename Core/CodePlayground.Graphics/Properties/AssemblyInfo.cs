ec1a084a56c92efcff1a33edbe530cbb610abd4d
yodasoda1219 <nora.github@bedafamily.com>
2023-09-29 16:24:28 -0700

    fix building on ios

--- Core/CodePlayground.Graphics/Properties/AssemblyInfo.cs
+++ Core/CodePlayground.Graphics/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using ObjCRuntime;
 #endif
 
 #if IOS
-[assembly: LinkWith("libSDL2.a", LinkTarget.x86_64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+//[assembly: LinkWith("libSDL2.a", LinkTarget = LinkTarget.Arm64 | LinkTarget.x86_64)]
 #endif
